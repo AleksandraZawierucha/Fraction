ee0f6f50bb031a056aa248da02846ade7223e608
Aleksandra Zawierucha <olala.zawierucha@gmail.com>
2021-04-25 14:39:48 +0200

    Add operator+ to the same denominators

--- include/fraction.hpp
+++ include/fraction.hpp
@@ -10,4 +10,11 @@ public:
     Fraction(int nominator_, int denominator_);
     int getNominator();
     int getDenominator();
+    Fraction operator+ (Fraction other)
+    {
+        if(other.getDenominator() == this->getDenominator())
+            return Fraction(other.getNominator()+this->getNominator(),other.getDenominator());
+        return Fraction(other.getNominator()*this->getDenominator() + this->getNominator()*other.getDenominator(), other.getDenominator()*this->getDenominator() );
+
+    }
 };
--- src/fractionTest.cpp
+++ src/fractionTest.cpp
@@ -26,3 +26,24 @@ TEST(FractionTest, ShortenFractionIfItIsPossibleWhenDenominatorIsBiggerThanNomin
     EXPECT_EQ(f.getNominator(), 2);
     EXPECT_EQ(f.getDenominator(), 1);
 }
+TEST(FractionTest, AddFractionWithTheSameDenominator)
+{
+    Fraction f1(1,2);
+    Fraction f2 = f1+Fraction(1,2);
+    EXPECT_EQ(f2.getNominator(), 1);
+    EXPECT_EQ(f2.getDenominator(), 1);
+}
+TEST(FractionTest, AddFractionWithDiferentDenominator)
+{
+    Fraction f1(1,2);
+    Fraction f2 = f1+Fraction(1,3);
+    EXPECT_EQ(f2.getNominator(), 5);
+    EXPECT_EQ(f2.getDenominator(), 6);
+}
+TEST(FractionTest, AddFractionWithDiferentDenominator2)
+{
+    Fraction f1(5,4);
+    Fraction f2 = f1+Fraction(7,8);
+    EXPECT_EQ(f2.getNominator(), 17);
+    EXPECT_EQ(f2.getDenominator(), 8);
+}
